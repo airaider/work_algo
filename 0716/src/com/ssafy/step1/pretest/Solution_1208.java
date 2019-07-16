352ff3ef686e831a179e43f6e7b8f816b7d88c22
Ki HK <airaider77@gmail.com>
2019-07-16 18:27:19 +0900

    homework

---
+++ 0716/src/com/ssafy/step1/pretest/Solution_1208.java
@@ -0,0 +1,34 @@
+package com.ssafy.step1.pretest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution_1208 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		for(int t=1;t<=10;++t) {
+			int T = Integer.parseInt(in.readLine());
+			String[] str = in.readLine().split(" ");
+			int size = str.length;
+		    int [] arr1 = new int [size];
+		    for(int j=0; j<size; ++j) {
+		         arr1[j] = Integer.parseInt(str[j]);
+		    }
+		    Arrays.sort(arr1);
+		    for(int i=0;i<T;++i) {
+		    	arr1[99]-=1;
+		    	arr1[0]+=1;
+		    	Arrays.sort(arr1);
+		    }
+		    int ans = arr1[99]-arr1[0];
+		    System.out.println("#"+t+" "+ans);
+		    
+		    
+		    
+		    
+		}
+
+}
+}
